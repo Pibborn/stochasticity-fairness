6bde130425106f14c8dfd18b6fe0b54a5907987b
Philipp Wolf <philippwolf99@icloud.com>
2025-11-07 16:25:04 +0100

    Adjusted job_scipts

--- src/job_script_array.sh
+++ src/job_script_array.sh
@@ -15,6 +15,18 @@ module load lang/Python/3.9.6-GCCcore-11.2.0
 module unload lang/SciPy-bundle/2021.10-foss-2021b
 
 #========[ + + + + Job Steps + + + + ]========#
+datasets=("adult" "compas" "german" "banks" "folktables_AK" "folktables_HI")
+models=("BinaryMI" "ICVAE")
+
+num_datatsets=${#datasets[@]}
+num_models=${#models[@]}
+
+dataset_index=$(( SLURM_ARRAY_TASK_ID / num_models))
+model_index=$(( SLURM_ARRAY_TASK_ID % num_models ))
+
+dataset=${datasets[$dataset_index]}
+model=${models[$model_index]}
+
 source  ../venv/bin/activate
-srun python3 decision_entropy_full_experiment.py --dataset=adult --model=BinaryMI --path=results
+srun python3 decision_entropy_full_experiment.py --dataset=$dataset --model=$model --path=results
 deactivate
