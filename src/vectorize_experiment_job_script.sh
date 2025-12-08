ad856e2c090ba928502b2a9013b6c9281b746eca
Philipp Wolf <philippwolf99@icloud.com>
2025-12-08 14:59:00 +0100

    Analyzed np.vectorize experiment

--- src/vectorize_experiment_job_script.sh
+++ src/vectorize_experiment_job_script.sh
@@ -16,5 +16,5 @@ module unload lang/SciPy-bundle/2021.10-foss-2021b
 
 #========[ + + + + Job Steps + + + + ]========#
 source  ../venv/bin/activate
-srun python3 decision_entropy_full_experiment.py --path=results
+srun python3 vectorize_experiment.py --path=results
 deactivate
